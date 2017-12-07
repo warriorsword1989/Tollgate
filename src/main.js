b812888e0f33e51391d740addd1a4b93411ef9a6
赵航 <18697479319@163.com>
2017-12-07 14:19:00 +0800

    增加element前端ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
