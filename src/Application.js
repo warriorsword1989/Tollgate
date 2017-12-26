573636da363749a02a7de0dd96cb86d8d520d145
赵航 <18697479319@163.com>
2017-12-26 20:36:07 +0800

    增加请求服务的接口文件

---
+++ src/Application.js
@@ -0,0 +1,27 @@
+/**
+ * @description 定义全局命名空间，并在此空间下定义属性和函数，以便全局使用
+ * @file Application.js
+ * @author    zhaohang
+ * @date      2017/12/26
+ *
+ * @copyright @Navinfo, all rights reserved.
+ */
+
+export const appConfig = {
+  appName: 'tollGate',
+  // serviceUrl: 'http://192.168.15.220:3000' // 正式环境的服务地址
+  // serviceUrl: 'http://192.168.4.189:3000' // 开发环境的服务地址
+  serviceUrl: 'http://localhost:3000' // 开发环境的服务地址
+};
+
+export const appUtil = {
+  setCurrentUser: function (user) {
+    sessionStorage.setItem(`${appConfig.appName}-user`, JSON.stringify(user));
+  },
+  getCurrentUser: function () {
+    return JSON.parse(sessionStorage.getItem(`${appConfig.appName}-user`));
+  },
+  removeCurrentUser: function () {
+    sessionStorage.removeItem(`${appConfig.appName}-user`);
+  }
+};
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,51 +3,51 @@
   <div style="width: 100%;height: 100%;overflow: auto;background-color: #FFFFFF" class="homeContainer">
     <el-tabs v-model="activeName"  :tab-position="tabPosition" style="height: 100%">
       <el-tab-pane label="照片作业" name="first" style="height: 100%">
-        <div class="scroll_style" style="height: 200px;">
+        <div class="scroll_style" style="height: 190px; padding-top: 20px">
           <div class="photoProject_rightDiv_search">
               <div class="photoProject_rightDiv_choose">
                 <span>行政区划：</span>
-                <el-select v-model="cityId" placeholder="请选择">
+                <el-select v-model="tip.adminCode" placeholder="请选择">
                   <el-option v-for="item in cityList" :key="item.value" :label="item.label" :value="item.value"></el-option>
                 </el-select>
               </div>
               <div class="photoProject_rightDiv_choose">
                 <span>版本号：</span>
                 <div style="display: inline-block">
-                  <el-input v-model="versionNum" placeholder="请输入内容"></el-input>
+                  <el-input v-model="tip.tipsVersion" placeholder="请输入内容"></el-input>
                 </div>
               </div>
               <div class="photoProject_rightDiv_choose">
                 <span>更新时间：</span>
-                <el-date-picker v-model="updateBeforeTime" type="date" placeholder="选择日期"format="yyyy-MM-dd" :picker-options="pickerupdateDateBefore"></el-date-picker>
+                <el-date-picker v-model="tip.updateStartTime" type="date" placeholder="选择日期"format="yyyy-MM-dd" :picker-options="pickerupdateDateBefore"></el-date-picker>
                 <span>至</span>
-                <el-date-picker v-model="updateAfterTime" type="date" placeholder="选择日期"format="yyyy-MM-dd" :picker-options="pickerupdateDateAfter"></el-date-picker>
+                <el-date-picker v-model="tip.updateEndTime" type="date" placeholder="选择日期"format="yyyy-MM-dd" :picker-options="pickerupdateDateAfter"></el-date-picker>
               </div>
               <div class="photoProject_rightDiv_choose">
                 <span>收费站名称：</span>
                 <div style="display: inline-block" >
-                  <el-input v-model="tollGateName" placeholder="请输入内容"></el-input>
+                  <el-input v-model="tip.tollName" placeholder="请输入内容"></el-input>
                 </div>
               </div>
-              <div>
-                <el-checkbox-group v-model="checkList"　class="photoProject_rightDiv_choose">
+              <div class="photoProject_rightDiv_choose">
+                <el-checkbox-group v-model="tip.isAdopted"　class="photoProject_rightDiv_choose">
                   <span>tips反馈：</span>
-                  <el-checkbox label="未处理"></el-checkbox>
-                  <el-checkbox label="已处理"></el-checkbox>
-                  <el-checkbox label="无法处理"></el-checkbox>
+                  <el-checkbox label='1'>未处理</el-checkbox>
+                  <el-checkbox label='2'>已处理</el-checkbox>
+                  <el-checkbox label='3'>无法处理</el-checkbox>
                 </el-checkbox-group>
               </div>
               <div>
-                <div style="float: right;margin-right: 100px" @click="searchList()">
+                <div style="float: right;margin-right: 100px" @click="getTollGateTipList()">
                   <el-button>查询</el-button>
                 </div>
               </div>
           </div>
         </div>
-        <div style="height:calc(100% - 300px);">
-          <el-table :data="photoData.slice((currentPage-1)*pagesize,currentPage*pagesize)" border height="" style="width: 100%;height: 100%;">
-            <el-table-column prop="id" label="序号" width="180" type="index" align="center"></el-table-column>
-            <el-table-column prop="tollGateName" label="收费站名称" width="180" align="center"></el-table-column>
+        <div style="height:calc(100% - 300px);padding-right: 10px">
+          <el-table :data="photoData.slice((currentPage-1)*pagesize,currentPage*pagesize)" border height="500 - 300" style="width: 100%;height: 100%;">
+            <el-table-column prop="id" label="序号" type="index" align="center"></el-table-column>
+            <el-table-column prop="tollGateName" label="收费站名称" align="center"></el-table-column>
             <el-table-column prop="tollGateType" label="收费站类型" align="center"></el-table-column>
             <el-table-column prop="passagewayNum" label="通道总数" align="center"></el-table-column>
             <el-table-column prop="provinceFee" label="是否跨省收费" align="center"></el-table-column>
@@ -65,74 +65,75 @@
         </div>
       </el-tab-pane>
 <!--情报作业-->
-      <el-tab-pane label="情报作业" name="second" style="height: 100%">
-        <div class="scroll_style" style="height: 200px;">
-          <div class="photoProject_rightDiv_search">
-            <div class="photoProject_rightDiv_choose">
-              <span>行政区划：</span>
-              <el-select v-model="cityId" placeholder="请选择">
-                <el-option v-for="item in cityList" :key="item.value" :label="item.label" :value="item.value"></el-option>
-              </el-select>
-            </div>
-            <div class="photoProject_rightDiv_choose">
-              <span>情报编码：</span>
-              <div style="display: inline-block">
-                <el-input v-model="versionNum" placeholder="请输入内容"></el-input>
-              </div>
-            </div>
-            <div class="photoProject_rightDiv_choose">
-              <span>新闻发布日期：</span>
-              <el-date-picker v-model="sendBeforeTime" type="date" placeholder="选择日期" :picker-options="sendDateBefore"></el-date-picker>
-              <span>至</span>
-              <el-date-picker v-model="sendAfterTime" type="date" placeholder="选择日期" :picker-options="sendDateAfter"></el-date-picker>
-            </div>
-              <el-checkbox-group class="photoProject_rightDiv_choose" v-model="checkList">
-                <span>完成状态：</span>
-                <el-checkbox label="未处理"></el-checkbox>
-                <el-checkbox label="已处理"></el-checkbox>
-                <el-checkbox label="无法处理"></el-checkbox>
-              </el-checkbox-group>
-            <div class="photoProject_rightDiv_choose">
-              <span>推送日期：</span>
-              <el-date-picker v-model="pushBeforeTime" type="date" placeholder="选择日期" :picker-options="pushBefore"></el-date-picker>
-              <span>至</span>
-              <el-date-picker v-model="pushAfterTime" type="date" placeholder="选择日期" :picker-options="pushAfter"></el-date-picker>
-            </div>
-            <div>
-              <div style="float: right;margin-right: 100px" @click="searchList()">
-                <el-button>查询</el-button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div  style="height:calc(100% - 300px);">
-          <div class="scroll_style">
-            <el-table  :data="genData.slice((currentPage-1)*pagesize,currentPage*pagesize)" border style="width: 100%;">
-              <el-table-column prop="id" label="序号" width="180" type="index" align="center"></el-table-column>
-              <el-table-column prop="tollGateName" label="收费站名称" width="180" align="center"></el-table-column>
-              <el-table-column prop="tollGateType" label="收费站类型" align="center"></el-table-column>
-              <el-table-column prop="passagewayNum" label="通道总数" align="center"></el-table-column>
-              <el-table-column prop="provinceFee" label="是否跨省收费" align="center"></el-table-column>
-              <el-table-column prop="feedback" label="tips反馈" align="center"></el-table-column>
-              <el-table-column prop="operation" label="操作" align="center">
-                <template slot-scope="scope">
-                  <el-button @click="handleClick(scope.row)" type="button" size="small">动态作业</el-button>
-                  <el-button @click="handleClick(scope.row)" type="button" size="small">静态作业</el-button>
-                </template>
-              </el-table-column>
-            </el-table>
-          </div>
-        </div>
-        <div class="block">
-          <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="currentPage" :page-size="4" layout="total, prev, pager, next" :total="20"></el-pagination>
-        </div>
-      </el-tab-pane>
+      <!--<el-tab-pane label="情报作业" name="second" style="height: 100%">-->
+        <!--<div class="scroll_style" style="height: 200px;">-->
+          <!--<div class="photoProject_rightDiv_search">-->
+            <!--<div class="photoProject_rightDiv_choose">-->
+              <!--<span>行政区划：</span>-->
+              <!--<el-select v-model="cityId" placeholder="请选择">-->
+                <!--<el-option v-for="item in cityList" :key="item.value" :label="item.label" :value="item.value"></el-option>-->
+              <!--</el-select>-->
+            <!--</div>-->
+            <!--<div class="photoProject_rightDiv_choose">-->
+              <!--<span>情报编码：</span>-->
+              <!--<div style="display: inline-block">-->
+                <!--<el-input v-model="versionNum" placeholder="请输入内容"></el-input>-->
+              <!--</div>-->
+            <!--</div>-->
+            <!--<div class="photoProject_rightDiv_choose">-->
+              <!--<span>新闻发布日期：</span>-->
+              <!--<el-date-picker v-model="sendBeforeTime" type="date" placeholder="选择日期" :picker-options="sendDateBefore"></el-date-picker>-->
+              <!--<span>至</span>-->
+              <!--<el-date-picker v-model="sendAfterTime" type="date" placeholder="选择日期" :picker-options="sendDateAfter"></el-date-picker>-->
+            <!--</div>-->
+              <!--<el-checkbox-group class="photoProject_rightDiv_choose" v-model="checkList">-->
+                <!--<span>完成状态：</span>-->
+                <!--<el-checkbox label="未处理"></el-checkbox>-->
+                <!--<el-checkbox label="已处理"></el-checkbox>-->
+                <!--<el-checkbox label="无法处理"></el-checkbox>-->
+              <!--</el-checkbox-group>-->
+            <!--<div class="photoProject_rightDiv_choose">-->
+              <!--<span>推送日期：</span>-->
+              <!--<el-date-picker v-model="pushBeforeTime" type="date" placeholder="选择日期" :picker-options="pushBefore"></el-date-picker>-->
+              <!--<span>至</span>-->
+              <!--<el-date-picker v-model="pushAfterTime" type="date" placeholder="选择日期" :picker-options="pushAfter"></el-date-picker>-->
+            <!--</div>-->
+            <!--<div>-->
+              <!--<div style="float: right;margin-right: 100px" @click="searchList()">-->
+                <!--<el-button>查询</el-button>-->
+              <!--</div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div  style="height:calc(100% - 300px);">-->
+          <!--<div class="scroll_style">-->
+            <!--<el-table  :data="genData.slice((currentPage-1)*pagesize,currentPage*pagesize)" border style="width: 100%;">-->
+              <!--<el-table-column prop="id" label="序号" width="180" type="index" align="center"></el-table-column>-->
+              <!--<el-table-column prop="tollGateName" label="收费站名称" width="180" align="center"></el-table-column>-->
+              <!--<el-table-column prop="tollGateType" label="收费站类型" align="center"></el-table-column>-->
+              <!--<el-table-column prop="passagewayNum" label="通道总数" align="center"></el-table-column>-->
+              <!--<el-table-column prop="provinceFee" label="是否跨省收费" align="center"></el-table-column>-->
+              <!--<el-table-column prop="feedback" label="tips反馈" align="center"></el-table-column>-->
+              <!--<el-table-column prop="operation" label="操作" align="center">-->
+                <!--<template slot-scope="scope">-->
+                  <!--<el-button @click="handleClick(scope.row)" type="button" size="small">动态作业</el-button>-->
+                  <!--<el-button @click="handleClick(scope.row)" type="button" size="small">静态作业</el-button>-->
+                <!--</template>-->
+              <!--</el-table-column>-->
+            <!--</el-table>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="block">-->
+          <!--<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="currentPage" :page-size="4" layout="total, prev, pager, next" :total="20"></el-pagination>-->
+        <!--</div>-->
+      <!--</el-tab-pane>-->
     </el-tabs>
   </div>
 
 </template>
 
 <script>
+    import { getTollGateTipList } from '../dataService/api';
     export default {
         name: "Home",
         data() {
@@ -172,23 +173,26 @@
             loading: true,
             activeName: 'first',
             tabPosition:'left',
-            versionNum:'',
-            updateBeforeTime:preTime,
-            updateAfterTime:time,
+            tip: {
+              tipsVersion:'',
+              updateStartTime:preTime,
+              updateEndTime:time,
+              tollName:'',
+              adminCode:'110000',
+              isAdopted: ['1', '2', '3'],
+            },
             sendBeforeTime:preTime,
             sendAfterTime:time,
             pushBeforeTime:preTime,
             pushAfterTime:time,
-            tollGateName:'',
             tollGateNames:[{}],
-            cityId:'',
             currentPage:1,
             pagesize:10,
             genDataPagesize:10,
             genDataCurrentPage:1,
             tollGateSearch:'',
             cityList: [{
-              value: '1',
+              value: '110000',
               label: '北京市'
             }, {
               value: '2',
@@ -310,13 +314,12 @@
               provinceFee:'11',
               feedback:'11',
             }],
-            checkList: ['',''],
             value: '',
             radio: '1',
             // 时间控件
             pickerupdateDateBefore:{
               disabledDate: (time) => {
-                let beginDateVal = this.updateAfterTime;
+                let beginDateVal = this.tip.updateEndTime;
                 if (beginDateVal) {
                   return time.getTime() > beginDateVal;
                 }
@@ -324,7 +327,7 @@
             },
             pickerupdateDateAfter:{
               disabledDate: (time) => {
-                let beginDateVal = this.updateBeforeTime;
+                let beginDateVal = this.tip.updateStartTime;
                 if (beginDateVal) {
                   return time.getTime() < beginDateVal;
                 }
@@ -390,16 +393,24 @@
             this.currentPage = val;
           },
           // 获取数据
-          // getUsers() {
-          //   let resource = this.$resource(this.url);
-          //   resource.query(this.filter)
-          //     .then((response) => {
-          //       this.photoData = response.data.datas;
-          //     })
-          //     .catch((response)=> {
-          //       this.$message.error('错了哦，这是一条错误消息');
-          //     });
-          // },
+          getTollGateTipList() {
+            const self = this;
+            const isAdoptedInt = [];
+            for (let i = 0; i < this.tip.isAdopted.length; i++) {
+              isAdoptedInt.push(parseInt(this.tip.isAdopted[i], 10));
+            }
+            const param = {
+              adminCode: this.tip.adminCode,
+              tipsVersion: this.tip.tipsVersion,
+              tollName: this.tip.tollName,
+              isAdopted: isAdoptedInt,
+              updateStartTime: this.tip.updateStartTime,
+              updateEndTime: this.tip.updateEndTime
+            };
+            getTollGateTipList(param).then(function (data) {
+
+            });
+          },
         },
     }
 </script>
@@ -408,7 +419,6 @@
 .photoProject_rightDiv_search{
   font-family:  Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
   font-size: 14px;
-  padding:25px 0;
   width: 100%;
 }
 .photoProject_rightDiv_search　span{
